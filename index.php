d99c45478a32746bec019cefaba6963be165e1d3
bryanstarman <67846938+bryanstarman@users.noreply.github.com>
2023-06-07 12:03:37 -0500

    logi

--- index.php
+++ index.php
@@ -1,59 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php require("comunes/head.php"); ?>
-
+<?php ?>
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <body style="background-color:white;">
 
-
-  <?php require("comunes/nav.php"); ?>
-
-  <!-- Header -->
-  <header>
-
-    <br><br> <br>
-    <form class="form-signin" action="login.php" method="POST" style=" background: rgb(0,51,26);
-background: linear-gradient(357deg, rgba(0,51,26,1) 0%, rgba(0,102,51,1) 50%, rgba(0,25,13,1) 100%); ">
-      <div style="text-align:center;color:red;font-weight:900">
-        <img src="img/profile.jpg" alt="" width="128px">
-        <?php
-        if (isset($_GET["error"])) {
-        ?>
-          <div class="alert" style="color: #b39523;color:white">
-            <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo "Usuario no existe"; ?> !
-          </div>
-        <?php
-        }
-        ?>
-
-        <?php
-        if (isset($_GET["iniciar"])) {
-        ?>
-            <div class="alert" style="background-color:#cb9a33; color:white">
-            <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo "Debe iniciar sesión"; ?> !
-          </div>
-        <?php
-        }
-        ?>
-
-
-
-
-        <h2 class="form-signin-heading" style="color: #b39523;">AUTENTICACIÓN</h2>
-        <input type="text" class="form-control" name="nombre_usuario" placeholder="Nombre de usuario" required="" autofocus="" />
-        <input type="password" class="form-control" name="clave" placeholder="Clave" required="" />
-
-        <button class="btn btn-lg btn-primary btn-block" type="submit" style="background-color: #b39523;">Aceptar</button>
-
-    </form>
-
-    </div>
-
-  </header>
-
-  
-
-  <?php require("comunes/scripts.php"); ?>
-
+<form>
+  <h1>Employer Log in</h1>
+  <div class="inset">
+    <p>
+      <label for="email">EMAIL ADDRESS</label>
+      <input type="text" name="email" id="email">
+    </p>
+    <p>
+      <label for="password">PASSWORD</label>
+      <input type="password" name="password" id="password">
+    </p>
+    <p>
+      <input type="checkbox" name="remember" id="remember">
+      <label for="remember">Remember me for 14 days</label>
+    </p>
+  </div>
+  <p class="p-container">
+    <span>Forgot password ?</span>
+    <input type="submit" name="go" id="go" value="Log in">
+  </p>
+</form>
 
 </body>
 
